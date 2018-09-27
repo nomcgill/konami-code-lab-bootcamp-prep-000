51393bacf633cb9a8b130bb1a20bc50aaa8e6ee2
Nolan McGill <nomcgill@gmail.com>
2018-09-27 03:17:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -19,6 +19,13 @@ const alphabet = ['a','b','c'];
 let index = 0
 oneKeyDownHandler(e){
   const key = e.key;
-  if(key === alphabet[index]){index++;
-  if (index===alphabet.length){alert("Hurray!)
-}
+  if(key === alphabet[index])
+    {index++;
+    if (index===alphabet.length)
+      {alert("Hurray!");
+      index = 0;
+      }
+    } else{
+      index = 0;
+      }
+  }
